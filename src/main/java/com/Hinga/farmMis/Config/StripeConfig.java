2732d374b740956985287fb075bc543b468c8b4f
aine1100 <149615514+aine1100@users.noreply.github.com>
2025-05-01 12:15:10 +0200

    updates

--- src/main/java/com/Hinga/farmMis/Config/StripeConfig.java
+++ src/main/java/com/Hinga/farmMis/Config/StripeConfig.java
@@ -6,6 +6,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StripeConfig {
     public StripeConfig(){
-        Stripe.apiKey="sk_test_51Oh5yiQnzXSA3KXtShx6AI2BWzWbUTjBTmHqCwnP8EDouKIp8C3027wPV4j6um6GLl4kS9RF2Q2obtmGihXQibQr00pQKWOH7Q";
+        Stripe.apiKey="";
     }
 }
